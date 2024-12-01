0dceff7758bda73b81a2fb75b54a57eea4853bee
basel <baselsoftwaredev@gmail.com>
2024-12-01 20:16:06 +0000

    move tab depend where the editor is

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,16 @@
 import * as vscode from 'vscode'
 
 export function activate (context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('extension.splitEditorRightLimited', () => {
+  const disposable = vscode.commands.registerCommand('extension.splitEditorRightLimited', async () => {
     const editorGroups = vscode.window.tabGroups.all
     const activeGroup = vscode.window.tabGroups.activeTabGroup
 
     if (editorGroups.length < 2) {
-      vscode.commands.executeCommand('workbench.action.splitEditorRight')
+      await vscode.commands.executeCommand('workbench.action.splitEditorRight')
     } else if (activeGroup.viewColumn === vscode.ViewColumn.Two) {
-      vscode.commands.executeCommand('workbench.action.moveEditorToFirstGroup')
+      await vscode.commands.executeCommand('moveActiveEditor', { to: 'left', by: 'group', value: 1 })
+    } else if (activeGroup.viewColumn === vscode.ViewColumn.One) {
+      await vscode.commands.executeCommand('moveActiveEditor', { to: 'right', by: 'group', value: 1 })
     } else {
       vscode.window.showInformationMessage('You can only have up to 2 editor groups.')
     }
