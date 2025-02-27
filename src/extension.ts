90bb601a613b657a4a9eb25b30d279ad5d7bc1d8
basel <baselsoftwaredev@gmail.com>
2025-02-27 22:32:26 +0000

    implement splitEditorRightLimited command to manage editor group splitting

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,14 @@
-import * as vscode from 'vscode'
+import * as vscode from 'vscode';
+import { splitEditorRightLimited } from './commands/split-editor';
 
-export function activate (context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('extension.splitEditorRightLimited', async () => {
-    const editorGroups = vscode.window.tabGroups.all
-    const activeGroup = vscode.window.tabGroups.activeTabGroup
-    const activeEditor = vscode.window.activeTextEditor
+export function activate(context: vscode.ExtensionContext) {
+  // Register splitEditorRightLimited command
+  const splitEditorDisposable = vscode.commands.registerCommand(
+    'extension.splitEditorRightLimited',
+    () => splitEditorRightLimited()
+  );
 
-    if (!activeEditor) {
-      vscode.window.showInformationMessage('No active editor found.')
-      return
-    }
-
-    if (editorGroups.length < 2) {
-      await vscode.commands.executeCommand('workbench.action.splitEditorRight')
-    } else if (activeGroup.viewColumn === vscode.ViewColumn.Two) {
-      await vscode.window.showTextDocument(activeEditor.document, vscode.ViewColumn.One)
-    } else if (activeGroup.viewColumn === vscode.ViewColumn.One) {
-      await vscode.window.showTextDocument(activeEditor.document, vscode.ViewColumn.Two)
-    } else {
-      vscode.window.showInformationMessage('You can only have up to 2 editor groups.')
-    }
-  })
-
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(splitEditorDisposable);
 }
 
-export function deactivate () { }
+export function deactivate() {}
